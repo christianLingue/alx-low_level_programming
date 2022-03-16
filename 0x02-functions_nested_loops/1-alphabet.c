9330770c9b830d89907bdbda55755fd562dd4a9e
christianLingue <christianklingue@gmail.com>
2022-03-15 19:36:29 -0700

    1-alphabet.c2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,13 @@
 #include "main.h"
+/**
+ * print alphabet - Print the alphabet in miniscule
+ */
 
 void print_alphabet(void)
 {
   char letter = 'a';
-  while( letter <= 'z' )
+  
+  while (letter <= 'z')
     {
       _putchar(letter);
       letter++;
