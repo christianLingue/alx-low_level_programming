5fbc13665abab6293d133fa844ac38dbd2da5f5d
christianLingue <christianklingue@gmail.com>
2022-03-22 19:02:06 -0700

    lc low level programming

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -38,8 +38,8 @@ char *_strcpy(char *dest, char *src)
 	return (dest);
 }
 
-/* another method using pointer notation
-
+/**
+ * another method using pointer notation
 char *_strcpy(char *dest, char *src)
 {
         while (*src)
